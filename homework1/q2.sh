bac735e8d08aade5570d698f84fd3765ca53ce26
l3lackcurtains <l3lackcurtains@protonmail.com>
2021-02-10 16:31:58 -0700

    sh file added

---
+++ homework1/q2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=arrays
+#SBATCH --output=/scratch/mp2525/arrays.out
+#SBATCH --time=20:00
+#SBATCH --workdir=/home/mp2525
+#SBATCH --mem=10000
+
+srun ./homework 2 sample_hw_dataset
